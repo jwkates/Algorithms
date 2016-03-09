e81418b65456c9ad434f14acdc8e6c61f7d1401d
Philip R. Onffroy <datamaniac18@gmail.com>
2016-03-09 13:07:27 -0500

    An addition of print

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main {
 	public static void main(String[] args) {
 		float pi = 3.141;
 		System.out.println("Pi is equal to " + pi + ". Everyone knows that.");
+                System.out.println(“This is the next best thing in middle of a print statement”);
+
 	}
 }
